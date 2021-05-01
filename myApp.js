5741c0606ed90cde9f0397a28e088e9a52a76e0c
Aidan Sabin <aidansabin@icloud.com>
2021-05-01 14:01:48 +0100

    added person model and schema
    lesson 2

--- myApp.js
+++ myApp.js
@@ -3,7 +3,14 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favouriteFoods: [String]
+});
+
+const Person = mongoose.model("Person", personScehma);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
