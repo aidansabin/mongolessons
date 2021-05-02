2dc8c72269f018408339f6113cdca9f7404d0cdc
Aidan Sabin <aidansabin@icloud.com>
2021-05-02 23:55:53 +0100

    create and save person
    lesson 3

--- myApp.js
+++ myApp.js
@@ -13,7 +13,15 @@ const personSchema = new Schema({
 const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  var aidanSabin = new Person({
+    name: "Aidan Sabin",
+    age: 22,
+    favoriteFoods: ['pizza', 'chocolate', 'burgers']
+  });
+  aidanSabin.save(err, data) {
+    if (err) return console.error(err);
+    done(null, data);
+  }
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
